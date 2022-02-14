ced209a6639e94a2355e865b2f6494e540bfef0d
Jaeheon-So <sjh-98@naver.com>
2022-02-15 01:05:04 +0900

    Add side navbar, search area

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Not yet
+// 제목, 내용 copyright 간격 조정하기
+// 사이드바 스타일 꾸미기
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
